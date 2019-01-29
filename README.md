4f9a1993c3b9d006eceea71e023c3a81707494ad
derrick eckardt <derrick.eckardt@gmail.com>
2019-01-29 20:28:07 +0000

    working with images

--- README.md
+++ README.md
@@ -1,25 +1,59 @@
 # a1
 
-Replace this with your report! (Or include a PDF).
+# Grading program
 
-To help you get started, we've included test images (one with a ground truth file) in test-images. We've also provided sample code in Python and C++
-to help you see how to do basic image operations in both languages. You can use either of these languages, or another general purpose language,
-as long as you write the image processing and vision routines yourselves.
+## Importing the image
+When key decision I made was to important the image as grayscale.
 
-To run the sample python code, just type:
+## Resizing the image.
+The images were 1700 x 2200 pixels.  Those are fairly large.  So, I played around with some reduced images sizes that I felt did not lose fidelity.  The benefit is that a smaller image would be computationally less intensive.  By reducing length and width by 50% each, makes my image only a fourth as large.  Higher values might be possible.
 
-```
-cd python-sample
-./sample.py
-```
+As a check, i did run itat 100% and got the same results.
+# FIX ME LATER
 
-To run the sample C++ code, type
-```
-cd c++-sample
-make
-./sample
-```
+## Filtering the images
+At the beginning, I decided to look at the intensities of the ink, and on the sample images, I found that almost all the pixels were within 20 of either 0 or 255 intensity-level, indicating that the pixels were all basically white or black.  For example, here is the distribution of intensities for test images a3.jpg:
 
 ```
-Sample block
-```
+{0: 125972,
+ 1: 30101,
+ 2: 18783,
+ 3: 15247,
+ 4: 11844,
+ 5: 8724,
+ 6: 5859,
+ 7: 3935,
+ 8: 2371,
+ 9: 1460,
+ 10: 744,
+ 11: 429,
+ 12: 213,
+ 13: 85,
+ 14: 34,
+ 15: 16,
+ 16: 3,
+ 17: 4,
+ 18: 1,
+ 236: 1,
+ 237: 2,
+ 238: 3,
+ 239: 10,
+ 240: 34,
+ 241: 147,
+ 242: 244,
+ 243: 506,
+ 244: 1208,
+ 245: 2455,
+ 246: 4132,
+ 247: 6942,
+ 248: 11665,
+ 249: 16356,
+ 250: 22534,
+ 251: 33716,
+ 252: 42046,
+ 253: 56994,
+ 254: 89774,
+ 255: 3225406}
+ ```
+
+Because of that, I ran a quick filter to change them all to 0 or 255, where I split them at 128.  This could potentially introduce some error for someone who did not fill them in very full.  Even for test-image a-48, which had some very light markings, splitting down the middle was still a good measure.
--- grade.py
+++ grade.py
@@ -27,21 +27,89 @@ import profile
 from PIL import Image
 from collections import Counter
 from pprint import pprint
+import numpy as np
+from timeit import timeit
 
-def grade(form, output_im, output_file):
-    print("Recognizing "+form+"...")
-    im = Image.open(form).convert('L')#.resize((680,880))
-    print(im.size)  # 1700x2200
-    px = im.load()
-    intensity_count = Counter()
+def add_edges(im,filter_matrix):
+    im_px = im.load()
+    fm_size = filter_matrix.shape
+    row_edges = int(fm_size[1]/2)
+    col_edges = int(fm_size[0]/2)
+    edged_im = Image.new("RGB", (im.width + 2*row_edges, im.height + 2*col_edges), color = (0,0,0))
+    # copy image over and add blank edges
+    for i in range(im.width):
+        for j in range(im.height):
+            edged_im.putpixel((i+row_edges,j+col_edges),im_px[i,j])#im.getpixel((i,j)))
+    # add left and right edges first
+    edged_im_px = edged_im.load()
+    for j in range(edged_im.height):
+        for i in range(row_edges):
+            edged_im.putpixel((i,j),edged_im_px[row_edges,j])
+            edged_im.putpixel((i+im.width+row_edges,j),edged_im_px[im.width+row_edges-1,j])
+    # add top and bottom edges, which copy the adjacent edge
+    edged_im_px = edged_im.load()
+    for i in range(edged_im.width):
+        for j in range(col_edges):
+            edged_im.putpixel((i,j),edged_im_px[i,col_edges])
+            edged_im.putpixel((i,im.height+col_edges+j),edged_im_px[i,im.height+col_edges-1])
+    return edged_im
 
+def convolute2(im,im_px, filter_matrix):
+    # flip matrix
+    filter_matrix = np.fliplr(np.flipud(filter_matrix)) 
+    edged_im = add_edges(im,filter_matrix)
+    edged_im_px = edged_im.load()
+    convol_row, convol_col = filter_matrix.shape
+    row_edges = int(convol_row/2)
+    col_edges = int(convol_col/2)
+    output_im = Image.new("RGB", (im.width, im.height), color = (0,0,0))
+    for i in range(im.width):
+        for j in range(im.height):
+            convol_total = np.array([0]*3)
+            for x in range(convol_row):
+                for y in range(convol_col):
+                    interim_1 = filter_matrix[x][y]
+                    interim_2 = edged_im_px[i+x,j+y]
+                    interim = np.multiply(interim_1,interim_2)
+                    convol_total = interim + convol_total
+            output_im.putpixel((i,j),(int(convol_total[0]),int(convol_total[1]),int(convol_total[2])))
+
+    return output_im
+
+def binary_image_filter(im, px):
     for x in range(im.width):
         for y in range(im.height):
             px[x,y] = 0 if px[x,y] < 128 else 255
-            intensity_count[px[x,y]] += 1
-    #resize image to simplify calculation
-    pprint(intensity_count)
+
+
+
+def grade(form, output_im, output_file):
+    print("Recognizing "+form+"...")
+    # resizing image
+    # importing as grayscale
+    im = Image.open(form).convert('L').resize((850,1100))
+    px = im.load()
+
+
+    # filter intensities
+    binary_image_filter(im,px)
+
+    # filter with gaussian to get rid of noise
+    # c - gaussian
+    gaussian_matrix = np.array([[0.003, 0.013, 0.022, 0.013, 0.003],
+                                [0.013, 0.059, 0.097, 0.059, 0.013],
+                                [0.022, 0.097, 0.159, 0.097, 0.022],
+                                [0.013, 0.059, 0.097, 0.059, 0.013],
+                                [0.003, 0.013, 0.022, 0.013, 0.003]])
+    im = convolute2(im,px,gaussian_matrix)
+    px = im.load()
+
+
+    # filter intensities again, to get rid of noise
+    binary_image_filter(im,px)
+
     im.save('resized_img.jpg')
+
     
 def output(output_file):
     print("Outputting to "+output_file)
