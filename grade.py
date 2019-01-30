1dd65f6c2716766b967fdc7fb25859ec5691f921
derrick eckardt <derrick.eckardt@gmail.com>
2019-01-30 01:06:01 +0000

    optimized to death

--- grade.py
+++ grade.py
@@ -23,48 +23,25 @@
 # Import libraries
 import sys
 import PIL
-import profile
+import cProfile
 from PIL import Image, ImageFilter
 from collections import Counter
 from pprint import pprint
 import numpy as np
 from timeit import timeit
 
-def add_edges(im,filter_matrix):
-    im_px = im.load()
-    fm_size = filter_matrix.shape
-    row_edges = int(fm_size[1]/2)
-    col_edges = int(fm_size[0]/2)
-    edged_im = Image.new("RGB", (im.width + 2*row_edges, im.height + 2*col_edges), color = (0,0,0))
-    # copy image over and add blank edges
-    for i in range(im.width):
-        for j in range(im.height):
-            edged_im.putpixel((i+row_edges,j+col_edges),im_px[i,j])#im.getpixel((i,j)))
-    # add left and right edges first
-    edged_im_px = edged_im.load()
-    for j in range(edged_im.height):
-        for i in range(row_edges):
-            edged_im.putpixel((i,j),edged_im_px[row_edges,j])
-            edged_im.putpixel((i+im.width+row_edges,j),edged_im_px[im.width+row_edges-1,j])
-    # add top and bottom edges, which copy the adjacent edge
-    edged_im_px = edged_im.load()
-    for i in range(edged_im.width):
-        for j in range(col_edges):
-            edged_im.putpixel((i,j),edged_im_px[i,col_edges])
-            edged_im.putpixel((i,im.height+col_edges+j),edged_im_px[i,im.height+col_edges-1])
-    return edged_im
-
-
 def add_edges_dict(im,filter_matrix):
     im_px = im.load()
+    im_width = im.width
+    im_height = im.height
     fm_size = [len(filter_matrix), len(filter_matrix[0])]
     row_edges = int(fm_size[1]/2)
     col_edges = int(fm_size[0]/2)
-    edged_im = Image.new("L", (im.width + 2*row_edges, im.height + 2*col_edges), color = 255)
+    edged_im = Image.new("L", (im_width + 2*row_edges, im_height + 2*col_edges), color = 255)
     edged_im_px = edged_im.load()
     # copy image over and add blank edges
-    for i in range(im.width):
-        for j in range(im.height):
+    for i in range(im_width):
+        for j in range(im_height):
             edged_im_px[i+row_edges,j+col_edges] = im_px[i,j]#im.getpixel((i,j)))
     # add left and right edges first
     edged_im_px = edged_im.load()
@@ -80,59 +57,32 @@ def add_edges_dict(im,filter_matrix):
             edged_im_px[i,im.height+col_edges+j]= edged_im_px[i,im.height+col_edges-1]
     return edged_im
 
-def convolute2(im,im_px, filter_matrix):
-    # flip matrix
-    # filter_matrix = np.fliplr(np.flipud(filter_matrix)) 
-    edged_im = add_edges(im,filter_matrix)
-    edged_im_px = edged_im.load()
-    convol_row, convol_col = filter_matrix.shape
-    row_edges = int(convol_row/2)
-    col_edges = int(convol_col/2)
-    output_im = Image.new("L", (im.width, im.height), color = 0)
-    for i in range(im.width):
-        for j in range(im.height):
-            convol_total = np.array([0]*3)
-            for x in range(convol_row):
-                for y in range(convol_col):
-                    interim_1 = filter_matrix[x][y]
-                    interim_2 = edged_im_px[i+x,j+y]
-                    interim = np.multiply(interim_1,interim_2)
-                    convol_total = interim + convol_total
-            output_im.putpixel((i,j),int(convol_total[0]))
-
-    return output_im
-
-def convolute3(im,im_px, filter_matrix):
-    # flip matrix
-    # filter_matrix = np.fliplr(np.flipud(filter_matrix)) 
+def convolute3(im,filter_matrix):
     edged_im = add_edges_dict(im,filter_matrix)
     edged_im_px = edged_im.load()
     convol_row, convol_col = len(filter_matrix), len(filter_matrix[0])
     row_edges = int(convol_row/2)
     col_edges = int(convol_col/2)
-    output_im = Image.new("L", (im.width, im.height), color = 0)
+    im_width = im.width
+    im_height = im.height
+    output_im = Image.new("L", (im_width, im_height), color = 0)
     output_im_px = output_im.load()
-    # print(output_im_px[0,0])
-    for i in range(im.width):
-        for j in range(im.height):
+    for i in range(im_width):
+        for j in range(im_height):
             convol_total = 0
             for x in range(convol_row):
                 for y in range(convol_col):
-#                    print(filter_matrix[x][y], type(filter_matrix[x][y]))
-#                    print(edged_im_px[i+x,j+y], type(edged_im_px[i+x,j+y]))
                     convol_total += filter_matrix[x][y] * edged_im_px[i+x,j+y]
             output_im_px[i,j] = int(convol_total)
 
     return output_im
 
-
-
 def binary_image_filter(im, px):
-    for x in range(im.width):
-        for y in range(im.height):
+    im_width = im.width
+    im_height = im.height
+    for x in range(im_width):
+        for y in range(im_height):
             px[x,y] = 0 if px[x,y] < 128 else 255
-
-
 
 def grade(form, output_im, output_file):
     print("Recognizing "+form+"...")
@@ -141,19 +91,15 @@ def grade(form, output_im, output_file):
     im = Image.open(form).convert('L').resize((850,1100))
     px = im.load()
 
-
     # filter intensities
-   # binary_image_filter(im,px)
+    binary_image_filter(im,px)
 
     # filter with gaussian to get rid of noise
-    box_blur_matrix = 1/9 * np.array([[1.0]*3]*3)
-
     box_blur_matrix_dict = {}
     for x in range(3):
         box_blur_matrix_dict[x] = {}
         for y in range(3):
             box_blur_matrix_dict[x][y] = 1.0/9.0
-
 
     # c - gaussian
     gaussian_matrix = np.array([[0.003, 0.013, 0.022, 0.013, 0.003],
@@ -161,19 +107,17 @@ def grade(form, output_im, output_file):
                                 [0.022, 0.097, 0.159, 0.097, 0.022],
                                 [0.013, 0.059, 0.097, 0.059, 0.013],
                                 [0.003, 0.013, 0.022, 0.013, 0.003]])
-    print(timeit(lambda: convolute3(im,px,box_blur_matrix_dict), number = 1))
-    print(timeit(lambda: im.filter(ImageFilter.Kernel((3,3),[1,1,1,1,1,1,1,1,1],9)), number =1))
 
-    
-    im = convolute3(im,px,box_blur_matrix_dict)
+    # print(timeit(lambda: convolute3(im,box_blur_matrix_dict), number = 1))
+    # print(timeit(lambda: im.filter(ImageFilter.Kernel((3,3),[1,1,1,1,1,1,1,1,1],9)), number =1))
+
+    im = convolute3(im,box_blur_matrix_dict)
     px = im.load()
-
 
     # filter intensities again, to get rid of noise
     binary_image_filter(im,px)
 
     im.save('resized_img.jpg')
-
     
 def output(output_file):
     print("Outputting to "+output_file)
@@ -187,5 +131,5 @@ def output(output_file):
 form, output_im, output_file = sys.argv[1:]
 
 # Grade form
-# profile.run("grade(form, output_im, output_file)")
+# cProfile.run("grade(form, output_im, output_file)")
 grade(form, output_im, output_file)
