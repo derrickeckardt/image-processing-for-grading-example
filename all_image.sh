e3fc0e0d150a41235818540c395fc11aa2fc6a96
derrick eckardt <derrick.eckardt@gmail.com>
2019-02-11 17:56:39 +0000

    Done with output file

--- all_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./grade2.py test-images/a-3.jpg a-3output.jpg output.txt
-./grade2.py test-images/a-27.jpg a-27output.jpg output.txt
-./grade2.py test-images/a-30.jpg a-30output.jpg output.txt
-./grade2.py test-images/a-48.jpg a-48output.jpg output.txt
-./grade2.py test-images/b-13.jpg b-13output.jpg output.txt
-./grade2.py test-images/b-27.jpg b-27output.jpg output.txt
-./grade2.py test-images/c-18.jpg c-18output.jpg output.txt
-./grade2.py test-images/c-33.jpg c-33output.jpg output.txt
