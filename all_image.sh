ab60f559d7fca6b1a8b6f4e648e29df58c80e918
derrick eckardt <derrick.eckardt@gmail.com>
2019-02-12 01:31:11 +0000

    Folder cleanup

--- all_image.sh
+++ all_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-./grade2.py test-images/a-3.jpg a-3output.jpg a-3output.txt
-./grade2.py test-images/a-27.jpg a-27output.jpg a-27output.txt
-./grade2.py test-images/a-30.jpg a-30output.jpg a-30output.txt
-./grade2.py test-images/a-48.jpg a-48output.jpg a-48output.txt
-./grade2.py test-images/b-13.jpg b-13output.jpg b-13output.txt
-./grade2.py test-images/b-27.jpg b-27output.jpg b-27output.txt
-./grade2.py test-images/c-18.jpg c-18output.jpg c-18output.txt
-./grade2.py test-images/c-33.jpg c-33output.jpg c-33output.txt
+./grade.py test-images/a-3.jpg a-3output.jpg a-3output.txt
+./grade.py test-images/a-27.jpg a-27output.jpg a-27output.txt
+./grade.py test-images/a-30.jpg a-30output.jpg a-30output.txt
+./grade.py test-images/a-48.jpg a-48output.jpg a-48output.txt
+./grade.py test-images/b-13.jpg b-13output.jpg b-13output.txt
+./grade.py test-images/b-27.jpg b-27output.jpg b-27output.txt
+./grade.py test-images/c-18.jpg c-18output.jpg c-18output.txt
+./grade.py test-images/c-33.jpg c-33output.jpg c-33output.txt
 ./all_grounds.sh
