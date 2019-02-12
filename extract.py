52ee0482ad97d345f2ff858093d81b98346665c7
derrick eckardt <derrick.eckardt@gmail.com>
2019-02-12 11:40:32 +0000

    progress on extract

--- extract.py
+++ extract.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # ./extract.py : Grading automated scanned answer sheet:
 #     ./extract.py injected.jpg output.txt 
@@ -22,11 +22,6 @@
 
 import sys
 import PIL
-
-
-
-import sys
-import PIL
 import cProfile
 from PIL import Image, ImageFilter
 from pprint import pprint
@@ -35,17 +30,23 @@ import math
 from random import choice
 from operator import itemgetter
 
-def get_answers(answers_file):
+def get_barcode(px, width):
+    # assuming relatively the same placement starting to the left of where the 
+    # middle should be
+    x, y = 50,450
+
+    front_spacer = 3
+    rear_spacer = 2
+    bar_width = 3
+
+    for i in range(width-x):
+        
+
+    barcode = []
+    return barcode
+    
+def decode(barcode):
     answers = {}
-    with open(answers_file, 'r') as file:
-        for line in file:
-            question, truth = int(line.split()[0]), line.split()[1]
-            answers[question] = {}
-            for option in 'ABCDE':
-                if option in truth:
-                    answers[question][option] = True
-                else:
-                    answers[question][option] = False
     return answers
 
 def print_barcode(answers,px):
@@ -92,27 +93,55 @@ def print_barcode(answers,px):
                 px[x+i,y+j] = color
         x += bar_width
         
-    print x
+    print(x)
 
-def inject(form, answers_file, injected_im):
-    # import answers
-    answers = get_answers(answers_file)
-    
-    # import form
-    im = Image.open(form).convert('RGB')
+def ref_points(px,width,height,location):
+    # find the location of reference points
+    # upper left postion, location = 0, or bottom right, location = -1
+    # 255 used to make sure mreference points to accidently noise
+    # modifieid from grade.py
+    for y in range(height*(-1*location)+location,height+(location*height)+location,1+2*location): # length
+        row_total = sum([1 for x in range(width) if px[x,y] < 255])
+        if row_total > 5:
+            break
+    for z in range(width*(-1*location)+location,width+(location*width)+location,1+2*location): # width
+        column_total = sum([1 for x in range(0,250) if px[z,x] < 255])
+        if column_total > 5:
+            break
+    return z,y
+
+def rotate_angle(rp1,rp2,cp1,cp2):
+    rp_ang = math.degrees(math.atan((rp2[1] - rp1[1])/(rp2[0] - rp1[0])))
+    cp_ang = math.degrees(math.atan((cp2[1] - cp1[1])/(cp2[0] - cp1[0])))
+    return rp_ang-cp_ang
+
+def extract(injected_im, output_txt):
+    # import injected form
+    im = Image.open(injected_im).convert('L')
     px = im.load()
     width, height = im.width, im.height
 
-    # print barcode
-    print_barcode(answers,px)
-    
-    #output file
-    im.save(injected_im)
-    
-    print("Barcode successfully outputted to '"+injected_im+"'.  Happy Test Taking!")
-    
-    im2 = Image.open(injected_im).convert('L')
-    im2.save("bw-"+injected_im)
+    # ensure form hasn't rotate, if it has, rotate it.  New reference points
+    rp1 = (0,0)
+    rp2 = (180,136)
+
+    # find reference points on original
+    cp1 = (0,0)
+    cp2 = ref_points(px,width,height,0)
+    print(cp1,cp2)
+
+    # finally, rotate the image
+    angle_to_rotate = rotate_angle(rp1,rp2,cp1,cp2)
+    im2 = im.rotate(angle_to_rotate)
+    px2 = im2.load()
+
+    # get barcode
+    barcode = get_barcode(px2, width)
+
+    # import answers
+    answers = decode(barcode)
+
+    print("Answers successfully extracted from '"+injected_im+"' and outputted into '"+output_txt+"'. Happy Grading!")
     
 ################################################################################
 # Run program
